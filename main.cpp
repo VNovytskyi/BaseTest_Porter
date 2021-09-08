9abde208ab4bd84f7d700d93bcb3f02a915fd44c
Vladyslav Novytskyi <novitskyi57@gmail.com>
2021-09-08 18:28:19 +0300

    Improve test

--- main.cpp
+++ main.cpp
@@ -1,83 +1,225 @@
 #include <QCoreApplication>
+#include <QRandomGenerator>
 #include <QDebug>
 
+#define USER_DATA_LENGTH 8
 extern "C" {
     #include "../../../Libs/porter/Source/porter.h"
 }
 
-#define USER_DATA_LENGTH 8
-porter_t porter;
-porter_t porter2;
+// Обвертка данных пользователя и проверка структуры пакета
+void testcase_wrap(); //Визуальная проверка 1
+void testcase_wrap2(); //Визуальная проверка 2
+void testcase_wrapAutoCheck(int iterationsCount); //Автоматическая проверка
 
-void init();           // Инициализация Porter`а
+//Проверка отправки и отправки по timeout
+void testcase_sendInSpace(int testTime);
 
-//Одиночные тесты
-void wrap1();          // Обвертка данных пользователя
-void unansweredSend(); // Отправка data без получения ack
-void answeredSend();   // Отправка data c получением искусственным ack
+//Отправка пакета Porter`ом и получение им исскуственного подтверждения
+void testcase_singleLoop();
 
-//Замкнутые тесты
-void sendAndRecv();
-void sendAndRecvInfinite();
+//Замыкание двух Porter`ов друг на друга и отправка одним пакета данных, а другим подтверждения
+void testcase_singleLoopReal();
 
-void sync();
+
+
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    qDebug() << "*** Test begin ***\n";
+    qDebug() << "(System) Test begin";
 
-    init();
-    //wrap1();
-    //unansweredSend();
-    //answeredSend();
+    testcase_wrap();
+    testcase_wrap2();
+    testcase_wrapAutoCheck(1'000'000);
+    testcase_sendInSpace(2'000);
+    testcase_singleLoop();
+    testcase_singleLoopReal();
 
-
-    for(uint16_t i = 0; i < 195; ++i) {
-        sendAndRecv();
-    }
-
-
-    /*
-    porter.tx_pack_id = 0;
-    sendAndRecvInfinite();
-    */
-
-   // sync();
-
-    qDebug() << "\n*** Test end ***";
+    qDebug() << "\n(System) Test end";
     return a.exec();
 }
 
-void init()
+void testcase_wrap()
 {
-    qDebug() << "\n***Init case";
+    qDebug() << "\n************************************ Wrap case ************************************";
+
+    porter_t porter;
     uint8_t init_result = porter_init(&porter, 200);
     if (init_result == PORTER_ERROR) {
         qDebug() << "[ ERROR ] Init porter";
-    } else {
-        qDebug() << "[ OK ] Init porter";
+        exit(-1);
     }
 
-    init_result = porter_init(&porter2, 200);
-    if (init_result == PORTER_ERROR) {
-        qDebug() << "[ ERROR ] Init porter2";
-    } else {
-        qDebug() << "[ OK ] Init porter2";
-    }
-}
-
-void wrap1()
-{
-    qDebug() << "\n***Wrap case";
     uint8_t porter_tx_buff[USER_DATA_LENGTH + PORTER_SERVICE_BYTES_COUNT];
-    uint8_t userData[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
+    uint8_t userData[USER_DATA_LENGTH] = { 1, 2, 3, 4, 5, 6, 7, 8 };
     porter_send(&porter, porter_tx_buff, userData, USER_DATA_LENGTH);
 
-    qDebug() << "User data:         " << QByteArray((char*)userData, USER_DATA_LENGTH).toHex('.');
+    qDebug() << "User data:            " << QByteArray((char*)userData, USER_DATA_LENGTH).toHex('.');
     qDebug() << "Wrappered data: " << QByteArray((char*)porter_tx_buff, USER_DATA_LENGTH + PORTER_SERVICE_BYTES_COUNT).toHex('.');
+
+    qDebug() << "Test case result: [ OK ]";
 }
 
+void testcase_wrap2()
+{
+    qDebug() << "\n************************************ Wrap case 2 ************************************";
+
+    porter_t porter;
+    uint8_t init_result = porter_init(&porter, 200);
+    if (init_result == PORTER_ERROR) {
+        qDebug() << "[ ERROR ] Init porter";
+        exit(-1);
+    }
+
+    uint8_t porter_tx_buff[USER_DATA_LENGTH + PORTER_SERVICE_BYTES_COUNT];
+    uint8_t userData[USER_DATA_LENGTH] = { 0x53, 0x12, 0x33, 0x34, 0x55, 0x26, 0x47, 0x08 };
+    porter_send(&porter, porter_tx_buff, userData, USER_DATA_LENGTH);
+
+    qDebug() << "User data:            " << QByteArray((char*)userData, USER_DATA_LENGTH).toHex('.');
+    qDebug() << "Wrappered data: " << QByteArray((char*)porter_tx_buff, USER_DATA_LENGTH + PORTER_SERVICE_BYTES_COUNT).toHex('.');
+    qDebug() << "Test case result: [ OK ]";
+}
+
+void testcase_wrapAutoCheck(int iterationsCount)
+{
+    qDebug() << "\n************************************ Auto check wrap ************************************";
+
+    porter_t porter;
+
+
+    uint8_t porter_tx_buff[USER_DATA_LENGTH + PORTER_SERVICE_BYTES_COUNT];
+    uint8_t userData[USER_DATA_LENGTH];
+
+    for (int i = 0; i < iterationsCount; ++i) {
+        int randomUserDataLength = QRandomGenerator::global()->bounded(1, 9);
+        for (int j = 0; j < randomUserDataLength; ++j) {
+            userData[j] =  QRandomGenerator::global()->bounded(0, 256);
+        }
+
+        uint8_t init_result = porter_init(&porter, 200);
+        if (init_result == PORTER_ERROR) {
+            qDebug() << "[ ERROR ] Init porter";
+            exit(-1);
+        }
+        porter_send(&porter, porter_tx_buff, userData, USER_DATA_LENGTH);
+
+        if (porter_tx_buff[0] != PORTER_DATA_PACK_ID || porter_tx_buff[1] != 0) {
+            qDebug() << "[ ERROR ] Porter pack header error";
+            exit(-1);
+        }
+
+        for (int j = 0; j < randomUserDataLength; ++j) {
+            if (porter_tx_buff[j + 2] != userData[j]) {
+                qDebug() << "[ ERROR ] User data in Porter package is damaged";
+                exit(-1);
+            }
+        }
+
+    }
+
+    qDebug() << "Iterations: " << iterationsCount;
+    qDebug() << "Test case result: [ OK ]";
+}
+
+void testcase_sendInSpace(int testTime)
+{
+    qDebug() << "\n************************************ Send in space case ************************************";
+
+    porter_t porter;
+    uint8_t init_result = porter_init(&porter, 100);
+    if (init_result == PORTER_ERROR) {
+        qDebug() << "[ ERROR ] Init porter";
+        exit(-1);
+    }
+
+    uint8_t porter_tx_buff[USER_DATA_LENGTH + PORTER_SERVICE_BYTES_COUNT];
+    uint8_t userData[USER_DATA_LENGTH];
+
+    int randomUserDataLength = QRandomGenerator::global()->bounded(1, 9);
+    for (int j = 0; j < randomUserDataLength; ++j) {
+        userData[j] =  QRandomGenerator::global()->bounded(0, 256);
+    }
+
+    porter_send(&porter, porter_tx_buff, userData, USER_DATA_LENGTH);
+    clock_t getData = 0;
+    clock_t testStartTime = clock();
+    while(1) {
+        porter_frame_t frame = porter_process(&porter, NULL, 0, clock());
+        if (frame.data != NULL && frame.length != 0) {
+            getData = clock();
+        }
+
+        if ((clock() - getData) > 150) {
+            qDebug() << "[ ERROR ] Porter must give the data to send. Timeout.";
+            exit(-1);
+        }
+
+        if ((clock() - testStartTime) > testTime) {
+            break;
+        }
+    }
+
+    qDebug() << "Test case result: [ OK ]";
+}
+
+void testcase_singleLoop()
+{
+    qDebug() << "\n************************************ Single loop case ************************************";
+
+    porter_t porter;
+    uint8_t init_result = porter_init(&porter, 200);
+    if (init_result == PORTER_ERROR) {
+        qDebug() << "[ ERROR ] Init porter";
+        exit(-1);
+    }
+
+    uint8_t porter_tx_buff[USER_DATA_LENGTH + PORTER_SERVICE_BYTES_COUNT];
+    uint8_t userData[USER_DATA_LENGTH];
+    int randomUserDataLength = QRandomGenerator::global()->bounded(1, 9);
+    for (int j = 0; j < randomUserDataLength; ++j) {
+        userData[j] =  QRandomGenerator::global()->bounded(0, 256);
+    }
+
+    porter_send(&porter, porter_tx_buff, userData, USER_DATA_LENGTH);
+
+    porter_frame_t frame = porter_process(&porter, NULL, 0, clock());
+    if (frame.data != NULL && frame.length != 0) {
+        if (porter_tx_buff[0] != PORTER_DATA_PACK_ID || porter_tx_buff[1] != 0) {
+            qDebug() << "[ ERROR ] Porter pack header error";
+            exit(-1);
+        }
+
+        for (int j = 0; j < randomUserDataLength; ++j) {
+            if (porter_tx_buff[j + 2] != userData[j]) {
+                qDebug() << "[ ERROR ] User data in Porter package is damaged";
+                exit(-1);
+            }
+        }
+
+    } else {
+        qDebug() << "[ ERROR ] Porter must give the data to send. Timeout.";
+        exit(-1);
+    }
+
+    uint8_t ack[1] = {PORTER_ACK_PACK_ID};
+    frame = porter_process(&porter, ack, 1, clock());
+    if (frame.data != NULL || frame.length != 0) {
+        qDebug() << "[ ERROR ] Porter get data to send after receive the ack";
+    }
+
+
+    qDebug() << "Test case result: [ OK ]";
+}
+
+void testcase_singleLoopReal()
+{
+    qDebug() << "\n************************************ Real single loop case ************************************";
+    //ccsa
+    qDebug() << "Test case result: [ OK ]";
+}
+
+/*
 void unansweredSend()
 {
     qDebug() << "\n***Unanswered send case";
@@ -227,3 +369,4 @@ void sync()
         }
     }
 }
+*/
