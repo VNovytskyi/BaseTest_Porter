34fd2e9280125dfdea06e36bbfb50237ff7cf403
Vladyslav Novytskyi <novitskyi57@gmail.com>
2021-09-08 16:11:18 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,110 +5,182 @@ extern "C" {
     #include "../../../Libs/porter/Source/porter.h"
 }
 
-#define DATA_LENGTH 8
-porter_t porter1;
+#define USER_DATA_LENGTH 8
+porter_t porter;
 porter_t porter2;
 
-uint8_t testSend1[] = {1, 2, 3, 4, 5, 6, 7, 8};
-void send1();
+void init();           // Инициализация Porter`а
 
-void initPorter();
-void deinitPorter();
+//Одиночные тесты
+void wrap1();          // Обвертка данных пользователя
+void unansweredSend(); // Отправка data без получения ack
+void answeredSend();   // Отправка data c получением искусственным ack
 
-void testSendCallback_Porter1(const uint8_t *data, const uint8_t length);
-void testSendCallback_Porter2(const uint8_t *data, const uint8_t length);
-
-void testRecvCallback_Porter1(const uint8_t *data, const uint8_t length);
-void testRecvCallback_Porter2(const uint8_t *data, const uint8_t length);
+//Замкнутые тесты
+void sendAndRecv();
+void sendAndRecvInfinite();
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     qDebug() << "*** Test begin ***\n";
 
-    initPorter();
+    init();
+    //wrap1();
+    //unansweredSend();
+    //answeredSend();
 
-    send1();
 
-
-    while(1) {
-        porter_process(&porter1, NULL, 0, clock());
+    for(uint16_t i = 0; i < 195; ++i) {
+        sendAndRecv();
     }
 
-    deinitPorter();
+
+    /*
+    porter.tx_pack_id = 0;
+    sendAndRecvInfinite();
+    */
 
     qDebug() << "\n*** Test end ***";
     return a.exec();
 }
 
-void testSendCallback_Porter1(const uint8_t *data, const uint8_t length)
+void init()
 {
-    QByteArray arr((char*)data, length);
-    qDebug() << "Porter1 send: " << arr.toHex('.');
-
-    porter_process(&porter2, data, length, clock());
-}
-
-void testSendCallback_Porter2(const uint8_t *data, const uint8_t length)
-{
-    QByteArray arr((char*)data, length);
-    qDebug() << "Porter2 send: " << arr.toHex('.');
-
-    porter_process(&porter1, data, length, clock());
-}
-
-void testRecvCallback_Porter1(const uint8_t *data, const uint8_t length)
-{
-    QByteArray arr((char*)data, length);
-    qDebug() << "Porter1 recv: " << arr.toHex('.');
-}
-
-void testRecvCallback_Porter2(const uint8_t *data, const uint8_t length)
-{
-    QByteArray arr((char*)data, length);
-    qDebug() << "Porter2 recv: " << arr.toHex('.');
-}
-
-void send1()
-{
-    uint8_t sendResult = porter_send(&porter1, testSend1, 8);
-    if (sendResult != PORTER_OK) {
-        qDebug() <<  "[ ERROR ] Porter send";
-        while(1);
+    qDebug() << "\n***Init case";
+    uint8_t init_result = porter_init(&porter, 200);
+    if (init_result == PORTER_ERROR) {
+        qDebug() << "[ ERROR ] Init porter";
     } else {
-        qDebug() <<  "[ OK ] Porter send";
+        qDebug() << "[ OK ] Init porter";
+    }
+
+    init_result = porter_init(&porter2, 200);
+    if (init_result == PORTER_ERROR) {
+        qDebug() << "[ ERROR ] Init porter2";
+    } else {
+        qDebug() << "[ OK ] Init porter2";
     }
 }
 
-void initPorter()
+void wrap1()
 {
-    uint8_t initResult = porter_init(&porter1, DATA_LENGTH, testSendCallback_Porter1, testRecvCallback_Porter1, 100);
-    if (initResult != PORTER_OK) {
-        qDebug() <<  "[ ERROR ] Porter1 init";
-        while(1);
-    } else {
-        qDebug() <<  "[ OK ] Porter1 init";
-    }
+    qDebug() << "\n***Wrap case";
+    uint8_t porter_tx_buff[USER_DATA_LENGTH + PORTER_SERVICE_BYTES_COUNT];
+    uint8_t userData[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
+    porter_send(&porter, porter_tx_buff, userData, USER_DATA_LENGTH);
 
-    uint8_t initResult2 = porter_init(&porter2, DATA_LENGTH, testSendCallback_Porter2, testRecvCallback_Porter2, 100);
-    if (initResult2 != PORTER_OK) {
-        qDebug() <<  "[ ERROR ] Porter2 init";
-        while(1);
-    } else {
-        qDebug() <<  "[ OK ] Porter2 init";
-    }
-
-    porter_set_tx_free_callback(&porter1, [](){
-        qDebug() << "Porter1 free tx";
-    });
-
-    porter_set_tx_free_callback(&porter2, [](){
-        qDebug() << "Porter2 free tx";
-    });
+    qDebug() << "User data:         " << QByteArray((char*)userData, USER_DATA_LENGTH).toHex('.');
+    qDebug() << "Wrappered data: " << QByteArray((char*)porter_tx_buff, USER_DATA_LENGTH + PORTER_SERVICE_BYTES_COUNT).toHex('.');
 }
 
-void deinitPorter()
+void unansweredSend()
 {
-    porter_deinit(&porter1);
-    porter_deinit(&porter2);
+    qDebug() << "\n***Unanswered send case";
+    uint8_t porter_tx_buff[USER_DATA_LENGTH + PORTER_SERVICE_BYTES_COUNT];
+    uint8_t userData[] = { 0x56, 0x12, 0x34, 0x24, 0x95, 0x56, 0x37, 0x18 };
+    porter_send(&porter, porter_tx_buff, userData, USER_DATA_LENGTH);
+
+    qDebug() << "User data:         " << QByteArray((char*)userData, USER_DATA_LENGTH).toHex('.');
+    qDebug() << "Wrappered data: " << QByteArray((char*)porter_tx_buff, USER_DATA_LENGTH + PORTER_SERVICE_BYTES_COUNT).toHex('.') << "\n";
+
+    while(1) {
+        porter_frame_t frame = porter_process(&porter, NULL, 0, clock());
+        if (frame.length > 0) {
+            qDebug() << "Want send: " << QByteArray((char*)frame.data, frame.length).toHex('.');
+        }
+    }
+}
+
+void answeredSend()
+{
+    qDebug() << "\n***Unanswered send case";
+    uint8_t porter_tx_buff[USER_DATA_LENGTH + PORTER_SERVICE_BYTES_COUNT];
+    uint8_t userData[] = { 0x56, 0x12, 0x34, 0x24, 0x95, 0x56, 0x37, 0x18 };
+    porter_send(&porter, porter_tx_buff, userData, USER_DATA_LENGTH);
+
+    qDebug() << "User data:            " << QByteArray((char*)userData, USER_DATA_LENGTH).toHex('.');
+    qDebug() << "Wrappered data: " << QByteArray((char*)porter_tx_buff, USER_DATA_LENGTH + PORTER_SERVICE_BYTES_COUNT).toHex('.') << "\n";
+
+    uint8_t test_ack = PORTER_ACK_PACK_ID;
+    //uint8_t test_ack = 65;
+    while(1) {
+        porter_frame_t frame = porter_process(&porter, &test_ack, 1, clock());
+        if (frame.length > 0) {
+            qDebug() << "Want send: " << QByteArray((char*)frame.data, frame.length).toHex('.');
+        }
+    }
+}
+
+void sendAndRecv()
+{
+    qDebug() << "\n***sendAndRecv case";
+    uint8_t porter_tx_buff[USER_DATA_LENGTH + PORTER_SERVICE_BYTES_COUNT];
+    uint8_t userData[] = { 0x56, 0x12, 0x34, 0x24, 0x95, 0x56, 0x37, 0x18 };
+    porter_send(&porter, porter_tx_buff, userData, USER_DATA_LENGTH);
+
+    qDebug() << "User data:            " << QByteArray((char*)userData, USER_DATA_LENGTH).toHex('.');
+    qDebug() << "Wrappered data: " << QByteArray((char*)porter_tx_buff, USER_DATA_LENGTH + PORTER_SERVICE_BYTES_COUNT).toHex('.') << "\n";
+
+    uint8_t porter_rx_buff[USER_DATA_LENGTH + PORTER_SERVICE_BYTES_COUNT];
+    uint8_t porter_rx_buff_length = 0;
+
+    uint8_t porter2_rx_buff[USER_DATA_LENGTH + PORTER_SERVICE_BYTES_COUNT];
+    uint8_t porter2_rx_buff_length = 0;
+
+    porter_frame_t frame, frame2;
+    for(uint16_t i = 0; i < 5000; ++i) {
+        frame = porter_process(&porter, porter_rx_buff, porter_rx_buff_length, clock());
+        if (frame.length > 0) {
+            memset(porter_rx_buff, 0, USER_DATA_LENGTH + PORTER_SERVICE_BYTES_COUNT);
+            qDebug() << "Want send: " << QByteArray((char*)frame.data, frame.length).toHex('.');
+            memcpy(porter2_rx_buff, frame.data, frame.length);
+            porter2_rx_buff_length = frame.length;
+        }
+
+        frame2 = porter_process(&porter2, porter2_rx_buff, porter2_rx_buff_length, clock());
+        if (frame2.length > 0) {
+            memset(porter2_rx_buff, 0, USER_DATA_LENGTH + PORTER_SERVICE_BYTES_COUNT);
+            qDebug() << "Want send2: " << QByteArray((char*)frame2.data, frame2.length).toHex('.');
+            memcpy(porter_rx_buff, frame2.data, frame2.length);
+            porter_rx_buff_length = frame2.length;
+        }
+    }
+}
+
+
+void sendAndRecvInfinite()
+{
+    qDebug() << "\n***sendAndRecv case";
+    uint8_t porter_tx_buff[USER_DATA_LENGTH + PORTER_SERVICE_BYTES_COUNT];
+    uint8_t userData[] = { 0x56, 0x12, 0x34, 0x24, 0x95, 0x56, 0x37, 0x18 };
+    porter_send(&porter, porter_tx_buff, userData, USER_DATA_LENGTH);
+
+    qDebug() << "User data:            " << QByteArray((char*)userData, USER_DATA_LENGTH).toHex('.');
+    qDebug() << "Wrappered data: " << QByteArray((char*)porter_tx_buff, USER_DATA_LENGTH + PORTER_SERVICE_BYTES_COUNT).toHex('.') << "\n";
+
+    uint8_t porter_rx_buff[USER_DATA_LENGTH + PORTER_SERVICE_BYTES_COUNT];
+    uint8_t porter_rx_buff_length = 0;
+
+    uint8_t porter2_rx_buff[USER_DATA_LENGTH + PORTER_SERVICE_BYTES_COUNT];
+    uint8_t porter2_rx_buff_length = 0;
+
+    porter_frame_t frame, frame2;
+    while(1) {
+        frame = porter_process(&porter, porter_rx_buff, porter_rx_buff_length, clock());
+        if (frame.length > 0) {
+            memset(porter_rx_buff, 0, USER_DATA_LENGTH + PORTER_SERVICE_BYTES_COUNT);
+            qDebug() << "Want send: " << QByteArray((char*)frame.data, frame.length).toHex('.');
+            memcpy(porter2_rx_buff, frame.data, frame.length);
+            porter2_rx_buff_length = frame.length;
+        }
+
+        frame2 = porter_process(&porter2, porter2_rx_buff, porter2_rx_buff_length, clock());
+        if (frame2.length > 0) {
+            memset(porter2_rx_buff, 0, USER_DATA_LENGTH + PORTER_SERVICE_BYTES_COUNT);
+            qDebug() << "Want send2: " << QByteArray((char*)frame2.data, frame2.length).toHex('.');
+            memcpy(porter_rx_buff, frame2.data, frame2.length);
+            porter_rx_buff_length = frame2.length;
+        }
+    }
 }
